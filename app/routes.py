30fedf694cacc0cc5b595d99cca9eb2e07ac2751
Shirish <shirish@transporter.city>
2018-07-18 14:15:52 +0530

    login form initiated

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,28 @@
 #from app directory app object of class flask
 from app import app
 from flask import render_template
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username' : "Gilmour"}
-    return render_template('index.html', title = 'Comfortably Numb', user = user)
+    posts = [
+        {
+            'author': {'username': 'Gangadhar'},
+            'body': 'Geeta Vishwas, mai hu Kilwish!'
+        },
+        {
+            'author': {'username': 'Ronaldo'},
+            'body': 'Juventus, I\'m coming!'
+        }
+    ]
+    return render_template('index.html', title = 'Comfortably Numb', user = user, posts = posts)
 
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html',
+                           title = 'Sign In',
+                           form = form)
